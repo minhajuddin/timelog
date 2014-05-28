00292aca4657dd44c191a8f3b0ebe79fb13ebe74
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2014-05-28 17:56:54 +0530

    Added ability to print current log file

--- main.go
+++ main.go
@@ -2,8 +2,23 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello")
+	fmt.Println(os.Args)
+
+	if len(os.Args) == 1 {
+		printLog(1)
+	}
+}
+
+func printLog(days int) {
+	f, err := os.Open("/home/minhajuddin/.timelog")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(-1)
+	}
+	io.Copy(os.Stdout, f)
 }
